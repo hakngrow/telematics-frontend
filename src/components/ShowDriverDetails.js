7621e57e33da5ff54a76d47a3e737f79ec1a4eae
hakngrow <56512250+hakngrow@users.noreply.github.com>
2021-07-13 17:00:32 +0800

    Update ShowDriverDetails.js

--- src/components/ShowDriverDetails.js
+++ src/components/ShowDriverDetails.js
@@ -14,7 +14,7 @@ class showDriverDetails extends Component {
   componentDidMount() {
     // console.log("Print id: " + this.props.match.params.id);
     axios
-      .get('http://localhost:8082/api/drivers/'+this.props.match.params.id)
+      .get('http://hakngrow-telematics-backend.herokuapp.com/api/drivers/'+this.props.match.params.id)
       .then(res => {
         // console.log("Print-showDriverDetails-API-response: " + res.data);
         this.setState({
