e60d0b6f882c7b4e6acdfa19944ca84c87889eb2
unknown <hakngrow@gmail.com>
2021-07-21 10:12:58 +0800

    Used https in http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const uri = process.env.REACT_APP_BACKEND_URI;
 
 export default axios.create({
-  baseURL: `http://${uri}/api`,
+  baseURL: `https://${uri}/api`,
   headers: {
     "Content-type": "application/json"
   }
